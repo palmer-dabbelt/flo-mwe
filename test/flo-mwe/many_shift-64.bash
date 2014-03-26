6dc513c5c1ec62d7c204655649efc6670e269eb0
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-25 22:31:23 -0700

    Add a shift test
    This is a small test for right-shifts.  This sort of pattern shows up
    a whole lot in the DES code but unfortunately appears to work OK.

---
+++ test/flo-mwe/many_shift-64.bash
@@ -0,0 +1,40 @@
+#include "tempdir.bash"
+#include "chisel-jar.bash"
+
+cat >test.scala <<EOF
+import Chisel._
+
+class test extends Module {
+  val io = new Bundle {
+    val o = Vec.fill(64){ UInt(OUTPUT, width = 1) }
+  }
+
+  val r_lo = Reg(init = UInt(5381, width = 32))
+  val lo = UInt(width = 27); lo := r_lo
+  r_lo := (lo << UInt(5)) + r_lo;
+
+  val r_hi = Reg(init = UInt(5381, width = 32))
+  val hi = UInt(width = 27); hi := r_hi
+  r_hi := (hi << UInt(5)) + r_hi + r_lo
+
+  val r = Cat(r_hi, r_lo)
+
+  for (i <- 0 until 64) { io.o(i) := r >> UInt(i) }
+}
+
+class tests(t: test) extends Tester(t) {
+  var cycle = 0
+  do {
+    step(1)
+    cycle += 1
+  } while (cycle < 10)
+}
+
+object test {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(args, () => Module(new test())) { t => new tests(t) }
+  }
+}
+EOF
+
+#include "harness.bash"
